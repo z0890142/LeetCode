f0fbbb8bdccb244e8c43c07aa0ffdf16dbd836e9
z0890142 <a0955117097@gmail.com>
2020-06-02 08:52:04 +0800

    add mergeTwoLists

---
+++ mergeTwoLists_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var mergeTwoListstest1 = ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val:  4,
+			Next: nil,
+		},
+	},
+}
+var mergeTwoListstest2 = ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 3,
+		Next: &ListNode{
+			Val:  4,
+			Next: nil,
+		},
+	},
+}
+
+var mergeTwoListstest3 = ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val:  4,
+			Next: nil,
+		},
+	},
+}
+var mergeTwoListstest4 = ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 5,
+		Next: &ListNode{
+			Val:  6,
+			Next: nil,
+		},
+	},
+}
+var mergeTwoListstest5 = ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 3,
+		Next: &ListNode{
+			Val:  4,
+			Next: nil,
+		},
+	},
+}
+
+func Test_mergeTwoLists(t *testing.T) {
+	tests := []struct {
+		l1     *ListNode
+		l2     *ListNode
+		result []int
+	}{
+		{
+			&mergeTwoListstest1,
+			&mergeTwoListstest2,
+			[]int{1, 1, 2, 3, 4, 4},
+		}, {
+			&mergeTwoListstest3,
+			nil,
+			[]int{1, 2, 4},
+		}, {
+			&mergeTwoListstest4,
+			&mergeTwoListstest5,
+			[]int{1, 1, 3, 4, 5, 6},
+		},
+	}
+	for _, testItem := range tests {
+		result := mergeTwoLists(testItem.l1, testItem.l2)
+		resultList := []int{}
+		getNode(result, &resultList)
+		assert.Equal(t, testItem.result, resultList)
+
+	}
+
+}
