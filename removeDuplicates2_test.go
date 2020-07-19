5e571e70b06613016d28d64585d7f1f5b5e72741
z0890142 <a0955117097@gmail.com>
2020-07-19 14:42:50 +0800

    0719

---
+++ removeDuplicates2_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_removeDuplicates2(t *testing.T) {
+	tests := []struct {
+		nums []int
+
+		result int
+		check  []int
+	}{
+		{
+			[]int{1, 1, 1, 2, 2, 3},
+			5,
+			[]int{1, 1, 2, 2, 3},
+		},
+		{
+			[]int{0, 0, 1, 1, 1, 1, 2, 3, 3},
+			7,
+			[]int{0, 0, 1, 1, 2, 3, 3},
+		},
+		{
+			[]int{0, 0, 1, 1, 1, 1, 2, 3, 3},
+			7,
+			[]int{0, 0, 1, 1, 2, 3, 3},
+		},
+	}
+
+	for _, testItem := range tests {
+		assert.Equal(t, testItem.result, removeDuplicates2(testItem.nums))
+
+	}
+}
---
+++ search_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_search(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		result bool
+	}{
+
+		{
+			[]int{4, 5, 6, 7, 0, 1, 2},
+			0,
+			true,
+		},
+		{
+			[]int{1, 1, 1, 1, 3},
+			3,
+			true,
+		},
+		{
+			[]int{1},
+			1,
+			true,
+		},
+		{
+			[]int{2, 5, 6, 0, 0, 1, 2},
+			0,
+			true,
+		},
+		{
+			[]int{2, 5, 6, 0, 0, 1, 2},
+			3,
+			false,
+		},
+	}
+
+	for _, testItem := range tests {
+		assert.Equal(t, testItem.result, search(testItem.nums, testItem.target))
+
+	}
+
+}
