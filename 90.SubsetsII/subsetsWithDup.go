79f1e95453e3586148b5bf9931582b4d6be4d051
z0890142 <a0955117097@gmail.com>
2021-01-07 21:26:04 +0800

    add 90

--- 90.SubsetsII/subsetsWithDup.go
+++ 90.SubsetsII/subsetsWithDup.go
@@ -11,8 +11,19 @@ func subsetsWithDup(nums []int) [][]int {
 	return result
 }
 
-func subsetsWithDupHelper(nums []int, tmp []int, result *[][]int) {
-	for index, val := range nums {
+func subsetsWithDupHelper(nums []int, preSlices []int, result *[][]int) {
+	for index := 0; index < len(nums); index++ {
+		preSlices = append(preSlices, nums[index])
+		tmp := make([]int, len(preSlices))
+		copy(tmp, preSlices)
+		*result = append(*result, tmp)
 
+		subsetsWithDupHelper(nums[index+1:], preSlices, result)
+
+		preSlices = preSlices[:len(preSlices)-1]
+
+		for index+1 < len(nums) && nums[index] == nums[index+1] {
+			index++
+		}
 	}
 }
